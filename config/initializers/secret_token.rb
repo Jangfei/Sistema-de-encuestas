d33eb9efd2fda954e95b95f2e1afbf93ef21860e
David Camilo Vasquez Lopez <DavCamVas@hotmail.com>
2013-04-13 12:01:16 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistemaEncuestas::Application.config.secret_token = '782d51b2756a44f5c21d010de41473cc35a6d643ab749c9fe80ff0e54d22a32765672a38c59115e56d252e00bc2efcbf80933147f44a9e638803864a398265cc'
