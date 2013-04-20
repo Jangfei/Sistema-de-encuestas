a930c8d3b6d39c29a8dea7b972028bddc982fc3b
David Camilo Vasquez Lopez <DavCamVas@hotmail.com>
2013-04-20 16:07:16 -0500

    Helpers generales de la aplicacion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def list_id
+    "list_#{suffix}"
+  end
+  
+  def footer_id
+    "footer_#{suffix}"
+  end
+  
+  def cancel_button_tag(url = "", &block)
+    button_tag t(:cancel), :type => "button", :class => (is_xhr? ? "remote_cancel_button" : "cancel_button"), :data => {:url => url}, &block
+  end
 end
