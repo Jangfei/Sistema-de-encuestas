4feb06dd40d9e81bf87ec523989fb1eb33cdd748
Erika S <erikita93_9@hotmail.com>
2013-04-20 13:08:03 -0500

    creaciones de asociaciones entre modelos

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ActiveRecord::Base
   attr_accessible :descripcion, :question_type_id, :user_id
+  has_many :answers
+  has many :options
+  belongs_to :question_type
+  belongs_to :user
+  
 end
--- app/models/question_type.rb
+++ app/models/question_type.rb
@@ -1,3 +1,5 @@
 class QuestionType < ActiveRecord::Base
   attr_accessible :descripcion
+  has_many :questions
+  
 end
