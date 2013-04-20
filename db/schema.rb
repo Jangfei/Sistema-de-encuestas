f72431ba71a0075c553bfd3813d804aba869f759
David Camilo Vasquez Lopez <DavCamVas@hotmail.com>
2013-04-20 15:02:17 -0500

    Arreglos en el layout

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,3 @@
-
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -117,4 +103,3 @@ ActiveRecord::Schema.define(:version => 20130420171602) do
   end
 
 end
->>>>>>> 91938913bbd0c0701d72ee851a5233d3e866c5ca
