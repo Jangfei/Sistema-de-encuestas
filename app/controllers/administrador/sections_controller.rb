74db458c6e42b27a9e76df18dd2d5373a54432f8
Cindy Ruiz <cindy_ruiz06@hotmail.com>
2013-04-21 00:19:25 -0500

    cambios en controller/administrador

--- app/controllers/administrador/sections_controller.rb
+++ app/controllers/administrador/sections_controller.rb
@@ -1,9 +1,8 @@
 class Administrador::SectionsController < ApplicationController
-<<<<<<< HEAD
+
   resources_controller
-=======
-  resource_controller
->>>>>>> fdbd87b487a87cd867b9ef99104e5525aae80808
+
+
   add_default_resource_methods
 end
 
