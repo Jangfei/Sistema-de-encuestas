3dc6ab07a4989543324386c7f0d3e230f0095b41
Erika S <erikita93_9@hotmail.com>
2013-04-24 02:03:45 -0500

    arreglo routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ SistemaEncuestas::Application.routes.draw do
       resources :questions
       resources :surveys
       resources :sections
-      resources :resolver_sureys
+      resources :resolved_sureys
 
     end
 
