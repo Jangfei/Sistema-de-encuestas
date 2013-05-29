ef36d8a61c33e958be75fa1165f498894b0b1f8b
David Camilo Vasquez Lopez <DavCamVas@hotmail.com>
2013-05-29 03:41:17 -0500

    Bug

--- app/controllers/administrador/resolved_surveys_controller.rb
+++ app/controllers/administrador/resolved_surveys_controller.rb
@@ -8,10 +8,10 @@ class Administrador::ResolvedSurveysController < ApplicationController
     
     answers = Answer.joins(:question => :question_type).joins(:resolved_survey).
       where("question_types" => {:descripcion => [QuestionType::OPCION_MULTIPLE, QuestionType::FALSO_VERDADERO]}, "resolved_surveys" => {:survey_id => parent_object}).
-      select("questions.descripcion as pregunta, answers.descripcion, COUNT(answers.descripcion) as cantidad").
-      group(["pregunta", "descripcion"])
+      select("questions.descripcion as pregunta, answers.descripcion as opcion, COUNT(answers.descripcion) as cantidad").
+      group(["pregunta", "opcion"])
       
-    data_array = [['Pregunta', 'Opcion', 'Cantidad']].concat(answers.map {|a| [a.pregunta, a.descripcion, a.cantidad]})          
+    data_array = [['Pregunta', 'Opcion', 'Cantidad']].concat(answers.map {|a| [a.pregunta, a.opcion, a.cantidad]})          
     @data =  data_array.to_json.html_safe
   end
 
